bd1a4796ced4ac290f41d749c4829dd5367dd24d
NicolasOBP <nicolasobp15@gmail.com>
2025-01-11 16:59:39 -0300

    initial layout

--- App.tsx
+++ App.tsx
@@ -3,7 +3,36 @@ import { StyleSheet, Text, View } from "react-native";
 import { useEffect } from "react";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+
+      <View style={styles.areaMoeda}>
+        <Text style={styles.titulo}>Selecione sua moeda</Text>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#101215",
+    paddingTop: 40,
+    alignItems: "center",
+  },
+  areaMoeda: {
+    backgroundColor: "#f9f9f9",
+    width: "90%",
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: 8,
+  },
+  titulo: {
+    fontSize: 16,
+    color: "#000000",
+    fontWeight: "500",
+    paddingLeft: 5,
+    paddingTop: 5,
+  },
+});
