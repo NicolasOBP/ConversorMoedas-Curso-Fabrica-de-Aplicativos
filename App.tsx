9c41823fad4dd366189ec478f3875b9c43aa0974
NicolasOBP <nicolasobp15@gmail.com>
2025-01-11 17:21:37 -0300

    feat: loading indicator

--- App.tsx
+++ App.tsx
@@ -1,19 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { PickerItem } from "./src/components/Picker";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "./src/services/api";
 
 export default function App() {
+  const [moedas, setMoedas] = useState([{}]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     async function loadMoedas() {
       const response = await api.get("all");
 
-      let arrayMoedas;
+      let arrayMoedas: Array<object> = [];
+
+      Object.keys(response.data).map((key) => {
+        arrayMoedas.push({
+          key: key,
+          label: key,
+          value: key,
+        });
+      });
+
+      setMoedas(arrayMoedas);
+      setLoading(false);
     }
 
     loadMoedas();
   }, []);
+
+  if (loading) {
+    return (
+      <View style={[styles.container, { justifyContent: "center" }]}>
+        <ActivityIndicator color="#FFF" size="large" />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
